c22421058ca37125630075c956fd18d6a18e91a3
EncryptSL <rydlos123@gmail.com>
2021-12-29 14:29:09 +0100

    Bump members

--- assets/app.js
+++ assets/app.js
@@ -1,4 +1,3 @@
-
     fetch("assets/data/members.json")
     .then(response => {
         response.json()
@@ -7,8 +6,17 @@
 
         json.forEach(element => {
             document.getElementById("members").innerHTML = `
-                ${element.username}
-            `            
+            <div class="col-lg-4">
+                <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#777"></rect><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text></svg>
+    
+            <h2>${element.username}</h2>
+                <p>${element.description}</p>
+                <p>
+                    <a class="btn btn-secondary" href="${element.twitch}">Twitch</a>
+                    <a class="btn btn-primary" href="${element.youtube}">Youtube</a>
+                </p>
+            </div>
+            `
         });
 
     })

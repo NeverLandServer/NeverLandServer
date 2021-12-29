5a9e464653b9f5e1050aad6909ce05a03f3411a8
EncryptSL <rydlos123@gmail.com>
2021-12-29 14:32:59 +0100

    Bump

--- assets/app.js
+++ assets/app.js
@@ -1,10 +1,8 @@
     fetch("assets/data/members.json")
     .then(response => {
-        response.json()
-    })
-    .then(json => {
-
-        json.forEach(element => {
+        json = response.json()
+        data = JSON.parse(json)
+        data.forEach(element => {
             document.getElementById("members").innerHTML = `
             <div class="col-lg-4">
                 <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#777"></rect><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text></svg>
@@ -18,5 +16,4 @@
             </div>
             `
         });
-
     })

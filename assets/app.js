bce2a04e2891e208a94af837151986efe1fb7cc7
EncryptSL <rydlos123@gmail.com>
2021-12-29 14:25:21 +0100

    Bump

--- assets/app.js
+++ assets/app.js
@@ -1,2 +1,14 @@
 
-    fetch("assets/data/members.json").then(response => response.json()).then(json => console.log(json))
+    fetch("assets/data/members.json")
+    .then(response => {
+        response.json()
+    })
+    .then(json => {
+
+        json.forEach(element => {
+            document.getElementById("members").innerHTML = `
+                ${element.username}
+            `            
+        });
+
+    })

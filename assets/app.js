20e965c9c36f7470d2eca170a38446bd47cee64f
EncryptSL <rydlos123@gmail.com>
2022-01-14 21:22:48 +0100

    Bump changed design

--- assets/app.js
+++ assets/app.js
@@ -14,9 +14,9 @@ document.addEventListener("DOMContentLoaded", function() {
         document.getElementById("membersInList").innerText = `Na serveru je ${data.members.length} členů`
         data.members.forEach(function (item) {
             document.getElementById("members").innerHTML += `
-            <div class="col-lg-4 mb-2">
+            <div class="col-lg-4 card-box">
                 <img class="rounded-circle" width="60" height="60" title="${item.username}" src="https://mc-heads.net/avatar/${item.username}">
-    
+
             <h2>${item.username}</h2>
                 <p><span class="badge bg-secondary text-white">${item.description}</span></p>
                 <p>

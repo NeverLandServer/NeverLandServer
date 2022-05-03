bae0a7bf9515af17fcf350d9f589d31ad74b600c
EncryptSL <Rydlos123@gmail.com>
2022-05-04 01:26:41 +0200

    Bump new web template..

--- assets/app.js
+++ assets/app.js
@@ -4,7 +4,7 @@ function socialButtons(link, btnName, clazzName) {
     else if (link === "#")
         return ``
     else
-        return `<a class="btn btn-sm ${clazzName}" target="_blank" href="${link}" title="${btnName}"><i class="fab fa-${btnName.toLowerCase()}"></i> ${btnName}</a>`
+        return `<a class="btn btn-sm ${clazzName}" target="_blank" href="${link}" title="${btnName}"><i class="bi bi-${btnName.toLowerCase()}"></i> ${btnName}</a>`
 }
 
 document.addEventListener("DOMContentLoaded", function() {
@@ -14,18 +14,32 @@ document.addEventListener("DOMContentLoaded", function() {
         document.getElementById("membersInList").innerText = `Na serveru je ${data.members.length} členů`
         data.members.forEach(function (item) {
             document.getElementById("members").innerHTML += `
-            <div class="col-lg-4 card-box py-2">
-                <img class="rounded-circle" width="60" height="60" title="${item.username}" src="https://mc-heads.net/avatar/${item.username}">
-
-            <h2>${item.username}</h2>
-                <p><span class="badge bg-secondary text-white">${item.description}</span></p>
-                <p>
-                    ${socialButtons(item.twitch, "Twitch", "btn-warning")}
-                    ${socialButtons(item.youtube, "Youtube", "btn-primary")}
-                    ${socialButtons(item.instagram, "Instagram", "btn-secondary")}
+            <div class="col-md-6 col-lg-3">
+            <div class="card bg-light">
+              <div class="card-body text-center">
+                <img
+                src="https://mc-heads.net/avatar/${item.username}"
+                  class="rounded-circle mb-3"
+                  alt="avt_${item.username}"
+                  title="${item.username}"
+                />
+                <h3 class="card-title mb-3">${item.username}</h3>
+                <p class="card-text">
+                    <span class="badge bg-secondary text-white">${item.description}</span>
                 </p>
+                ${socialButtons(item.twitch, "Twitch", "btn-warning")}
+                ${socialButtons(item.youtube, "Youtube", "btn-primary")}
+                ${socialButtons(item.instagram, "Instagram", "btn-secondary")}
+              </div>
             </div>
+          </div>
             `
         })
+    })
+    fetch('assets/data/members.json').then((response) => {
+        return response.json();
+    }).then((data) => {
+        var rand = Math.floor(Math.random()*data.members.length);
+        document.getElementById("randomAv").innerHTML = `<img class="avatar" src="https://visage.surgeplay.com/bust/${data.members[rand].uuid}">`
     })
 })

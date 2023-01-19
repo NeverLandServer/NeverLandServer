9a32cba8aa086ce2eece6e19eb6846c73af68bb6
EncryptSL <Rydlos123@gmail.com>
2023-01-19 01:58:25 +0100

    Removed snowfall

--- pages/index.js
+++ pages/index.js
@@ -7,24 +7,12 @@ import About from './/components/about';
 import Faq from './/components/faq';
 import History from './/components/history';
 import Members from './/components/members';
-import Snowfall from 'react-snowfall'
 import {React, useEffect, useState } from 'react';
+import Stats from './components/stats';
 
 export default function Home() {
-
-  const [snow, setSnow] = useState("")
-
-  const getSnow = () => {
-    setSnow(<Snowfall color='white' snowflakeCount={200}/>)
-  }
-
-  useEffect(() => {
-    getSnow()
-  }, [])
-
   return (
     <>
-      {snow}
       <Head/>
       <Navbar />
       <Header />
@@ -33,6 +21,7 @@ export default function Home() {
       <About />
       <History />
       <Faq />
+      <Stats />
       <Members />
     </>
   )

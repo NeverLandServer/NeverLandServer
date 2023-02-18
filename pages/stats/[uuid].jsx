89719d7921b0247ccac134f01b8a9601e7334ec2
EncryptSL <Rydlos123@gmail.com>
2023-02-18 04:11:43 +0100

    Update [uuid].jsx

--- pages/stats/[uuid].jsx
+++ pages/stats/[uuid].jsx
@@ -8,7 +8,7 @@ const PlayerPage = ({ player }) => {
         <div className="container col-md-9 col-lg-10 px-md-4 my-3">
             <section className="p-1 mb-4 bg-light rounded-3">
                 <div className="container-fluid py-5">
-                    <h5 className="display-5 fw-bold">{player?.username} #{player.id}</h5>
+                    <h5 className="display-5 fw-bold">{player?.username} #{player?.id}</h5>
                     <p className="col-md-8 fs-4"><Link href="/" className="text-link">ZPĚT</Link></p>
                 </div>
             </section>
@@ -19,7 +19,7 @@ const PlayerPage = ({ player }) => {
                     <div className="card mb-3 text-center">
                         <div className="card-header text-bg-dark">AKTUÁLNÍ SKIN</div>
                         <div className="card-body">
-                            <img src={"https://visage.surgeplay.com/full/200/" + player.uuid} className="rounded" />
+                            <img src={"https://visage.surgeplay.com/full/200/" + player?.uuid} className="rounded" />
                         </div>
                     </div>
                 </div>
@@ -30,19 +30,19 @@ const PlayerPage = ({ player }) => {
                             <div className="row">
                                 <div className="col-4">
                                     <div className="font-size-h3 font-w500">
-                                        <span>{player.blocks}</span>
+                                        <span>{player?.blocks}</span>
                                     </div>
                                     <p className="text-muted mt-2 mb-0">Položené bloky</p>
                                 </div>
                                 <div className="col-4">
                                     <div className="font-size-h3 font-w500">
-                                        <span>{player.kills}</span>
+                                        <span>{player?.kills}</span>
                                     </div>
                                     <p className="text-muted mt-2 mb-0">Zabití</p>
                                 </div>
                                 <div className="col-4">
                                     <div className="font-size-h3 font-w500">
-                                        <span>{player.deaths}</span>
+                                        <span>{player?.deaths}</span>
                                     </div>
                                     <p className="text-muted mt-2 mb-0">Úmrtí</p>
                                 </div>
